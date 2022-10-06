c381b34fb824e35c7e0f8e7a23233354f9daa637
Pedro Seixas <pedrodfs15@gmail.com>
2022-10-05 21:41:15 -0400

    Added Arduino Code Which Communicates With WebApp Using SerialPort

---
+++ WebApp/LEDWebApp/LEDWebApp.ino
@@ -0,0 +1,72 @@
+#include <FastLED.h>
+#define NUM_LEDS 64
+#define LED_PIN 2
+CRGB leds[NUM_LEDS];
+
+class Panel{
+
+public:
+  int leds_status[NUM_LEDS];
+  int i;
+  Panel(){
+    for(i=0;i<NUM_LEDS;i++){
+      leds_status[i] = 0;
+    }
+  }
+void button(char Px, char Py){
+    int intX, intY;
+    intX = Px - 48;
+    intY = Py - 48;
+
+    for(int i=0; i<8;i++){
+      if(intY==i && intX<8){
+        leds[intX+(8*i)] = CRGB:: Red;
+      }
+    }
+    FastLED.show();
+  }
+ void show_led(){
+    int i;
+      for(i=0;i<NUM_LEDS;i++){
+
+        if(leds_status[i] == 1){
+          leds[i] = CRGB:: Red;
+
+          }else if(leds_status[i] == 0){
+            leds[i] = CRGB:: Black;
+
+          }
+    }
+    FastLED.show();
+   }
+};
+
+void setup() {
+
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(200);
+  Serial.setTimeout(100);
+
+
+}
+
+void loop() {
+
+ Panel panel;
+ unsigned char xLoc, yLoc;
+ String buttonInfo;
+ 
+ if(Serial.available()){
+  buttonInfo = Serial.readString();
+   yLoc = buttonInfo[4];
+   xLoc = buttonInfo[6];
+   panel.button(xLoc,yLoc);
+   if ((xLoc == '9') && (yLoc == '9')){
+    for(int i = 0; i<NUM_LEDS;i++){
+      leds[i] = CRGB:: Black;
+    }
+    FastLED.show();
+   }
+}
+}
