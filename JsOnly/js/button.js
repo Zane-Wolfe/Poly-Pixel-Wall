50eeab09b74b004e6d9a92aadae603be5cafe87b
Pedro Seixas <pedrodfs15@gmail.com>
2022-10-14 15:41:38 -0400

    Updated comments

--- JsOnly/js/button.js
+++ JsOnly/js/button.js
@@ -5,6 +5,8 @@ var button = {};
 
 function createButtons(row, col){
  var count = 0;
+
+ //creating array of objects and inserting the button object
  for(var i=0; i<row; i++){
   but_arr[i] = []
    for(var j=0; j<col; j++){
@@ -17,6 +19,7 @@ function createButtons(row, col){
 
       but_arr[i][j] = button;
 
+      //Creating the buttons on the index.html with the attributes
       newBut.setAttribute("class", "but");
       newBut.setAttribute("id", "but_"+i+"_"+j);
       newBut.setAttribute("onClick", "changeColor(this.id)");
@@ -27,23 +30,28 @@ function createButtons(row, col){
  }
  document.getElementById("layout").style.gridTemplateColumns = "repeat("+row+", 60px)";
 }
+
 createButtons(8,8);
 
 console.log(but_arr);
 
 function changeColor(elid){
 
+  //Taking button position from button id
   var row = parseInt(elid[4]);
   var col = parseInt(elid[6]);
 
   console.log(row, col);
   console.log(elid);
-  
+
+  //Check if button is active, if false it and turn background
+  // to red
   if(but_arr[row][col].active == false){
       document.getElementById(elid).style.background= "red";
       //socket.emit('lights', {status:elid});
       but_arr[row][col].active = true;
-      
+  // Check if button is NOT active, if true change the background to
+  // white
   }else if(but_arr[row][col].active == true){
       document.getElementById(elid).style.background= "white";
       //socket.emit('lights', {status:elid});
@@ -53,6 +61,7 @@ function changeColor(elid){
 }
 
 function reset(elid){
+  //TODO also deactivate all the buttons
   console.log(elid);
   elements = document.getElementsByClassName("but");
   for(var i=0; i<elements.length; i++){
@@ -63,8 +72,20 @@ function reset(elid){
 
 }
 
+function isActive(elid){
+
+
+}
+function activateButton(elid){
+
+}
+
+function deactivateButton(elid){
+
+}
 //socket.on('data', function(data){
 
   //console.log(data);
 
 //});
+
