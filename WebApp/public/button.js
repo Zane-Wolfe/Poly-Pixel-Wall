d51dc22a16b7751cee8973665220add175eeb5d2
Pedro Seixas <pedrodfs15@gmail.com>
2022-11-12 12:33:34 -0500

    Created New Function To Emit Socket & Optimized Code

--- WebApp/public/button.js
+++ WebApp/public/button.js
@@ -35,39 +35,27 @@ createButtons(8,8);
 
 console.log(but_arr);
 
-function changeColor(elid, color){
+function changeColor(elid){
 
   var row = parseInt(elid[4]);
   var col = parseInt(elid[6]);
+  var color = "#B4A5A5";
 
   console.log(row, col);
   console.log(elid);
   
-<<<<<<< HEAD
   if(isActive(row, col)==false){
     //Select color from color picker
-    document.getElementById(elid).style.background= document.getElementById('colorPicker').value;
-    socket.emit('lights', {status:elid});
+    color = document.getElementById('colorPicker').value;
+    document.getElementById(elid).style.background = color ;
+    emitSignal(elid, row, col, color, "true");
     activateButton(row, col);
 
 // Check if button is NOT active, if true change the background to
 // white
 }else if(isActive(row, col)){
-    document.getElementById(elid).style.background= "#B4A5A5";
-=======
-  if(!isActive(elid, row, col)){
-    console.log(color);
     document.getElementById(elid).style.background= color;
-    socket.emit('lights', {status:elid});
-    activateButton(elid, row, col);
-
-// Check if button is active, if true change the background to
-// white
-
-}else if(isActive(elid, row, col)){
-    document.getElementById(elid).style.background= "white";
->>>>>>> f26fe537693028ba3e1bdbdc67ba763bf7a3881a
-    socket.emit('lights', {status:elid});
+    emitSignal(elid, row, col, color, "false");
     deactivateButton(row, col);
   }
   console.log(but_arr[row][col].active);
@@ -77,6 +65,7 @@ function changeColor(elid, color){
 function reset(elid){
 
   elements = document.getElementsByClassName("but");
+
   for(var i=0; i<elements.length; i++){
     elements[i].style.background = "#B4A5A5";
   }
@@ -87,9 +76,6 @@ function reset(elid){
       but_arr[i][j].active = false;
     }
   }
-  //document.getElementById(elid).style.background = "white";
-
-  socket.emit('lights', {status:elid});
 
 }
 
@@ -115,46 +101,25 @@ function activateButton(row, col){
 function deactivateButton(row, col){
   but_arr[row][col].active = false;
 }
-<<<<<<< HEAD
-=======
 
-//Function to create JSON object
-//TODO Send it to the arduino
+function applyAll(){
 
-function sendJson(elid, color){
+  elements = document.getElementsByClassName("but");
+  color = document.getElementById('colorPicker').value;
+  for(var i=0; i<elements.length; i++){
+    elements[i].style.background = color;
+  }
 
-  var obj = new Object();
-  obj.id = elid; 
-  obj.color = color; 
-  
-  var stringObj = JSON.stringify(obj);
-
-  return stringObj;
-  
+  for(var i=0; i<8; i++){
+    for(var j=0; j<8; j++){
+      but_arr[i][j].active = true;
+      emitSignal("0", i, j, color, true);
+    }
+  }
 }
-// function physicalButton(data){
-//   var text = data;
-//   var button_id = text.id;
-//   var button_color = text.color;
-//   var row = text.x;
-//   var col = text.y;
 
-//   console.log(data)
-//   console.log(button_id);
-//   console.log(text.color);
-//   console.log(row);
-//   console.log(col);
-
->>>>>>> f26fe537693028ba3e1bdbdc67ba763bf7a3881a
+function emitSignal(elid, _row, _col, _color, _status){
+  socket.emit('lights', {id:elid, row: _row, col: _col, color: _color, status: _status})
+}
 
 // }
-socket.on('json', function(data){
-
-console.log(data);
-var color = data.color;
-var id = data.id;
-//var row = data.row;
-// var col = data.col;
-changeColor(id,color);
-
-});
